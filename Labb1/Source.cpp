aa6fb080b31e87c1ed2d392c3e82d7ea74993d49
Vitryssen <andre1.nordlund@gmail.com>
2021-04-03 10:49:37 +0200

    Added BFS and DFS to check if complete matrix

--- Labb1/Source.cpp
+++ Labb1/Source.cpp
@@ -1,29 +1,63 @@
 #include <iostream>
+#include <list>
 #include "reader.h"
 #include "printer.h"
 #include "convert.h"
 using namespace std;
 void DFS(int start, int goal, int size, vector<bool>& visited);
+void BFS(int start, adjacency_list_t test);
 void resetVisited(vector<bool>& visited);
 int allVisited(vector<bool>& visited, int start);
 int allNodesReachable(adjacency_list_t test, vector<bool> visited);
-bool validMatrix(adjacency_list_t test, vector<bool> visited);
+bool verifyMatrix(adjacency_list_t test, vector<bool> visited);
+bool validMatrix(adjacency_list_t test);
 std::vector<std::vector<double> > matrix;
 double totWeight = 0;
 bool incomplete = false;
 int main() {
 	adjacency_list_t test =	parse_file("export.txt");
-	matrix = listToMatrix(test);
+	BFS(0, test);
+	/*if (validMatrix) {
+		matrix = listToMatrix(test);
+	}
+	else {
+		return 0;
+	}*/
+}
+void BFS(int start, adjacency_list_t test) {
 	vector<bool> visited(test.first.size(), false);
-	if (validMatrix(test, visited)) {
-		cout << "Valid matrix" << endl;
-
+	list<int> queue;
+	visited[start] = true;
+	queue.push_back(start);
+	list<int>::iterator it;
+	for (auto j = test.second.begin(); j != test.second.end(); j++) {
+		cout << "Pair " << j[0].n1 << " and " << j[0].n2 << endl;
+	}
+	/*while (!queue.empty())
+	{
+		start = queue.front();
+		//cout << start << " ";
+		queue.pop_front();
+		for (auto j = test.second.begin(); j != test.second.end(); j++) {
+			if (j[0].n2 == start) {
+				if (!visited[j[0].n1]) {
+					visited[j[0].n1] = true;
+					queue.push_back(j[0].n1);
+					cout << " At node " << j[0].n2 << " adding node " << j[0].n1 << " to the queue" << endl;
+				}
+			}
+		}
+	}*/
+}
+bool validMatrix(adjacency_list_t test) {
+	vector<bool> visited(test.first.size(), false);
+	if (verifyMatrix(test, visited)) {
+		return true;
 	}
 	else
-		cout << "Not valid matrix, node that failed " << allNodesReachable(test, visited) << endl;
-
+		return false;
 }
-bool validMatrix(adjacency_list_t test, vector<bool> visited) {
+bool verifyMatrix(adjacency_list_t test, vector<bool> visited) {
 	for (int x = 0; x < test.first.size(); x++) {
 		for (int i = 0; i < test.first.size(); i++) {
 			DFS(i, x, test.first.size(), visited);
--- Source.cpp
+++ Source.cpp
@@ -1,33 +1,71 @@
 #include <iostream>
+#include <list>
 #include "reader.h"
 #include "printer.h"
 #include "convert.h"
 using namespace std;
 void DFS(int start, int goal, int size, vector<bool>& visited);
+int BFS(int start, adjacency_list_t test);
 void resetVisited(vector<bool>& visited);
-int allVisited(vector<bool>& visited, int start);
+int allVisited(vector<bool>& visited);
 int allNodesReachable(adjacency_list_t test, vector<bool> visited);
-bool validMatrix(adjacency_list_t test, vector<bool> visited);
+bool verifyMatrix(adjacency_list_t test, vector<bool> visited);
+bool validMatrix(adjacency_list_t test);
 std::vector<std::vector<double> > matrix;
 double totWeight = 0;
 bool incomplete = false;
 int main() {
-	adjacency_list_t test =	parse_file("export.txt");
+	adjacency_list_t test = parse_file("export.txt");
 	matrix = listToMatrix(test);
+	if (BFS(0, test) == -1) {
+		cout << "BFS valid" << endl;
+	}
+	else {
+		cout << "BFS not valid" << endl;
+	}
+	if (validMatrix(test)) {
+		cout << "DFS valid" << endl;
+	}
+	else {
+		cout << "DFS not valid" << endl;
+	}
+	return 0;
+}
+int BFS(int start, adjacency_list_t test) {
 	vector<bool> visited(test.first.size(), false);
-	if (validMatrix(test, visited)) {
-		cout << "Valid matrix" << endl;
-
+	list<int> queue;
+	visited[start] = true;
+	queue.push_back(start);
+	list<int>::iterator it;
+	while (!queue.empty())
+	{
+		start = queue.front();
+		//cout << start << " ";
+		queue.pop_front();
+		for (auto j = test.second.begin(); j != test.second.end(); j++) {
+			if (j[0].n1 == start) {
+				if (!visited[j[0].n2]) {
+					visited[j[0].n2] = true;
+					queue.push_back(j[0].n2);
+				}
+			}
+		}
+	}
+	return allVisited(visited);
+}
+bool validMatrix(adjacency_list_t test) {
+	vector<bool> visited(test.first.size(), false);
+	if (verifyMatrix(test, visited)) {
+		return true;
 	}
 	else
-		cout << "Not valid matrix, node that failed " << allNodesReachable(test, visited) << endl;
-
+		return false;
 }
-bool validMatrix(adjacency_list_t test, vector<bool> visited) {
+bool verifyMatrix(adjacency_list_t test, vector<bool> visited) {
 	for (int x = 0; x < test.first.size(); x++) {
 		for (int i = 0; i < test.first.size(); i++) {
 			DFS(i, x, test.first.size(), visited);
-			if (allVisited(visited, i) != -1) {
+			if (allVisited(visited) != -1) {
 				return false;
 			}
 			resetVisited(visited);
@@ -39,7 +77,7 @@ int allNodesReachable(adjacency_list_t test, vector<bool> visited) {
 	for (int x = 0; x < test.first.size(); x++) {
 		for (int i = 0; i < test.first.size(); i++) {
 			DFS(i, x, test.first.size(), visited);
-			int valid = allVisited(visited, i);
+			int valid = allVisited(visited);
 			if (valid != -1) {
 				return valid;
 			}
@@ -48,12 +86,12 @@ int allNodesReachable(adjacency_list_t test, vector<bool> visited) {
 	}
 	return -1;
 }
-void resetVisited(vector<bool> &visited) {
+void resetVisited(vector<bool>& visited) {
 	for (int i = 0; i < visited.size(); i++) {
 		visited.at(i) = false;
 	}
 }
-int allVisited(vector<bool>& visited, int start) {
+int allVisited(vector<bool>& visited) {
 	for (int i = 0; i < visited.size(); i++) {
 		if (!visited.at(i)) {
 			return i;
